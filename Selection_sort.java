a0c8bd37e586d89ceecfe1f727f524bef5f11d14
deshan manodya <91672656+demshano@users.noreply.github.com>
2022-03-18 23:24:52 +0530

    Update Selection_sort.java

--- Selection_sort.java
+++ Selection_sort.java
@@ -24,3 +24,30 @@ for(int i=0;i<a.length;i++){
 }
 	}
 }
+
+/*
+the report of selection sort:
+
+here the array elements {1,5,8,2,3,9,7}
+
+the number of passes of selection sort is (array elements -1). so 
+outer for loop we initalize (i=0;i<array.length-1;i++). for the conviniant
+we declare a variable which is for i=0; min=i;
+now we  declare the inner loop which is normal conditions.
+here we have to focuse, we initialize j=i+1; just becuase after crashing the 
+inner loop for the first time, again inner loop start j=0. in that case 
+sorting process can not continue. after crashing the inner loop
+we have to modify each inner loop after crashing.
+inside the inner loop we use the if condition is there a number which is 
+fulfill  this condition : array[min]>array[j] which is the real meaning of  is,
+ there is a number which is less than min number. however there is a 
+number, we hold min=j;
+
+after we swap with min = i  and min =j outside the inner loop after
+ fully done of inner loop.
+continue that process. 
+
+after we print new sorted array by using new for loop.  
+
+
+*/
